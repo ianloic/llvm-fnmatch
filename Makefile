013a076d9cdaabc71bbee1b2534854029fda73dd
Ian McKellar <ian@mckellar.org>
2009-06-03 10:44:36 -0400

    add performance tests

--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ CPPFLAGS = $(shell llvm-config --cxxflags) -Wall -Werror -g -O0
 LDFLAGS = $(shell llvm-config --ldflags --libs core jit native)
 LD = $(CXX)
 
-all: fnmatch-test
+all: fnmatch-test fnmatch-benchmark
 	
 fnmatch-test: fnmatch-test.o fnmatch.o fnmatch-parse.o
 	$(CXX) -o fnmatch-test fnmatch-test.o fnmatch.o fnmatch-parse.o $(LDFLAGS)
 
+fnmatch-benchmark: fnmatch-benchmark.o fnmatch.o fnmatch-parse.o
+	$(CXX) -o fnmatch-benchmark fnmatch-benchmark.o fnmatch.o fnmatch-parse.o $(LDFLAGS)
+
 clean:
-	rm -f fnmatch-test fnmatch.o fnmatch-parse.o
+	rm -f fnmatch-test fnmatch-benchmark fnmatch-test.o fnmatch-benchmark.o fnmatch.o fnmatch-parse.o
---
+++ fnmatch-benchmark.cpp
@@ -0,0 +1,65 @@
+#include "fnmatch.h"
+
+#include <fnmatch.h>
+#include <sys/time.h>
+
+#define PATTERN "*.txt"
+#define PATH "hello.txt"
+#define RUNS 1000000
+
+class Timer {
+  public:
+    void start() { gettimeofday(&_start, NULL); }
+    void stop() { gettimeofday(&_stop, NULL); }
+    double seconds() {
+      return (double)(_stop.tv_sec-_start.tv_sec) + 
+        (double)(_stop.tv_usec-_start.tv_usec) / (double)10000000;
+    }
+  private:
+    struct timeval _start;
+    struct timeval _stop;
+};
+
+static const char* test_patterns[] = {
+  "", "?", "*", "1", ".", "foo", "f?o", "f[aeiou]o", "*.txt", "*.*",
+  NULL
+};
+
+static const char* test_paths[] = {
+  "", "a", "foo", "hello", "hello.txt", "hello.txto",
+  NULL
+};
+
+int main(int argc, char**argv) {
+  Timer setup;
+  setup.start();
+  // important initialization - do this first
+  FnmatchCompiler::Initialize();
+
+  FnmatchCompiler* compiler = new FnmatchCompiler();
+  compiler->Compile(PATTERN);
+  compiler->optimize();
+  setup.stop();
+
+  Timer llvm_impl, libc_impl;
+  int i;
+
+  llvm_impl.start();
+  for (i=0; i<RUNS; i++) {
+    compiler->run(PATH);
+  }
+  llvm_impl.stop();
+
+  libc_impl.start();
+  for (i=0; i<RUNS; i++) {
+    fnmatch(PATTERN, PATH, 0);
+  }
+  libc_impl.stop();
+
+  printf ("testing %d times...\n", RUNS);
+  printf ("llvm setup took %lf seconds\n", setup.seconds());
+  printf ("llvm runs took %lf seconds\n", llvm_impl.seconds());
+  printf ("libc runs took %lf seconds\n", libc_impl.seconds());
+
+}
+
