bbc6a64f973eca4aa3d2329f17c2a089616d69d9
Ian McKellar <ian@mckellar.org>
2009-06-05 15:23:27 -0400

    time llvm setup with llvm run, also show percentages as well as times

--- fnmatch-benchmark.cpp
+++ fnmatch-benchmark.cpp
@@ -22,34 +22,36 @@ class Timer {
 };
 
 int main(int argc, char**argv) {
-  Timer setup;
-  setup.start();
-
-  FnmatchCompiler* compiler = new FnmatchCompiler();
-  compiler->Compile(PATTERN);
-  compiler->optimize();
-  setup.stop();
-
   Timer llvm_impl, libc_impl;
   int i;
 
+  // test our LLVM-based fnmatch
   llvm_impl.start();
+  FnmatchCompiler* compiler = new FnmatchCompiler();
+  compiler->Compile(PATTERN);
+  compiler->optimize();
   for (i=0; i<RUNS; i++) {
     compiler->run(PATH);
   }
   llvm_impl.stop();
 
+  // test the system libc implementation
   libc_impl.start();
   for (i=0; i<RUNS; i++) {
     fnmatch(PATTERN, PATH, 0);
   }
   libc_impl.stop();
 
-  printf ("testing %d times...\n", RUNS);
-  printf ("llvm setup took %lf seconds\n", setup.seconds());
-  printf ("llvm runs took %lf seconds\n", llvm_impl.seconds());
-  printf ("libc runs took %lf seconds\n", libc_impl.seconds());
+  // how'd that go?
+  printf("testing %d times...\n", RUNS);
+  printf("llvm runs took %lf seconds\n", llvm_impl.seconds());
+  printf("libc runs took %lf seconds\n", libc_impl.seconds());
 
-  //compiler->dump();
+  double ratio = llvm_impl.seconds() / libc_impl.seconds();
+  if (ratio > 1.0) {
+    printf("llvm runs %lf%% slower\n", 100 * (ratio - 1.0));
+  } else {
+    printf("libc runs %lf%% slower\n", 100 * ((1.0/ratio) - 1.0));
+  }
 }
 
