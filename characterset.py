08cdead6c7ec9f953a669ee81a8e84ae7eb9298f
Ian McKellar <ian@mckellar.org>
2009-07-06 16:30:50 -0400

    improve docs, tests for CharacterSet

--- characterset.py
+++ characterset.py
@@ -1,23 +1,38 @@
 class CharacterSet:
+  '''represents a set of characters, defined either by what is included in the
+  set or by what is excluded from the set'''
+  
   def __init__(self, inclusive, characters):
+    '''call CharacterSet.including(), CharacterSet.excluding() or
+    CharacterSet.range() instead of this'''
     self.inclusive = bool(inclusive)
     self.characters = set(characters)
 
   @classmethod
   def including(klass, characters):
+    '''construct a new CharacterSet including just the provided characters'''
     return klass(True, characters)
 
   @classmethod
   def excluding(klass, characters):
+    '''construct a new CharacterSet containing all characters except the ones
+    provided'''
     return klass(False, characters)
 
   @classmethod
   def range(klass, start, end):
+    '''construct a new CharacterSet containing all characters in the range (inclusive)'''
+    assert isinstance(start, basestring) and len(start) == 1
+    assert isinstance(end, basestring) and len(end) == 1
+    
     if end < start:
       start, end = end, start
-    return klass(True, [chr(c) for c in range(ord(start), ord(end))])
+    return klass(True, [chr(c) for c in range(ord(start), ord(end)+1)])
 
   def __contains__(self, c):
+    '''is the supplied character in the set?'''
+    assert isinstance(c, basestring) and len(c) == 1
+    
     if self.inclusive:
       return c in self.characters
     else:
@@ -25,6 +40,7 @@ class CharacterSet:
 
   @property
   def label(self):
+    '''when displaying this set to the user, how should we present it?'''
     # sort the list of characters
     chars = list(self.characters)
     chars.sort()
@@ -33,15 +49,21 @@ class CharacterSet:
     else:
       if not chars: return 'ANY'
       else: return 'NOT '+`''.join(chars)`
+      
   def __eq__(self, other):
+    assert isinstance(other, CharacterSet)
     return self.inclusive == other.inclusive and \
         self.characters == other.characters
   def __ne__(self, other):
+    assert isinstance(other, CharacterSet)
     return not (self == other)
   def __hash__(self):
     return hash('%s %s' % (self.inclusive, ''.join(self.characters)))
 
   def union(self, other):
+    '''set union'''
+    assert isinstance(other, CharacterSet)
+    
     if self.inclusive and other.inclusive:
       return CharacterSet.including(self.characters.union(other.characters))
     elif not self.inclusive and not other.inclusive:
@@ -52,6 +74,9 @@ class CharacterSet:
       return CharacterSet.excluding(self.characters-other.characters)
 
   def __sub__(self, other):
+    '''set difference'''
+    assert isinstance(other, CharacterSet)
+    
     if self.inclusive and other.inclusive:
       return CharacterSet.including(self.characters-other.characters)
     elif not self.inclusive and not other.inclusive:
@@ -64,28 +89,36 @@ class CharacterSet:
       raise 'wtf?'
 
   def intersection(self, other):
+    '''set intersection'''
+    assert isinstance(other, CharacterSet)
+    
     return self - (self - other)
 
   def all(self):
-    '''matches all characters'''
+    '''does the set match all characters?'''
     return not self.inclusive and not self.characters
 
   def empty(self):
-    '''matches no characters'''
+    '''does the set match no characters?'''
     return self.inclusive and not self.characters
 
   def __repr__(self):
     return 'CharacterSet(%s, %s)' % (`self.inclusive`, `''.join(self.characters)`)
 
   def disjoint(self, other):
+    '''does the set not intersect the other?'''
+    assert isinstance(other, CharacterSet)
     return (self.intersection(other)).empty()
+    
   def intersects(self, other):
+    '''does the set intersect another?'''
+    assert isinstance(other, CharacterSet)
     return not self.disjoint(other)
 
 
 def __disjoin(charsets, charset):
   '''@charsets is a set of disjoint charsets, @charset is a charset.
-  return a set of charsets with the same range as all inputs, but disjoint'''
+  return a set of charsets with the same range as all inputs, but all disjoint'''
   # prove preconditions
   assert isinstance(charsets, set)
   for cs in charsets:
@@ -103,28 +136,21 @@ def __disjoin(charsets, charset):
 
 
 def distinctCharacterSets(orig_charsets):
-  '''for a set of charsets, find a set of disjoint charsets
-  whose union is equal to the union of the original charsets'''
+  '''from the original set of charsets find a new set of charsets such that:
+   * the union of the original charsets is equal to the union of the new 
+     charsets
+   * none of the new character sets intersect
+   * each original charset is equal to the union of one or more new charsets
+  '''
+  
   # original_charsets contains the set of original charsets
   charsets = set(orig_charsets)
 
+  # find disjoint sets
   partition = reduce(__disjoin, charsets, set())
-
-  #print '\ndistinctCharacterSets(%s)' % `charsets`
-  # partition will hold the partition of the union of those sets
-  #print `charsets`
-#  partition = set((charsets.pop(),))
-#  #print `charsets`
-#  for o in charsets:
-#    new_partition = set()
-#    for p in partition:
-#      new_partition = new_partition.union(set((p-o, o-p, o.intersection(p))))
-#    partition = new_partition
 
   # filter the empty set out of the partition
   partition = set([charset for charset in partition if not charset.empty()])
-
-  #print 'partitioned %s into %s' % (`orig_charsets`, `partition`)
 
   # check that our result matches our contract
   # make sure that none of our character sets intersect
