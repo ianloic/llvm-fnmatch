adbe4c8d73188b0496ab8084d1429a1f37562b76
Ian McKellar <ian@mckellar.org>
2009-07-07 22:13:05 -0400

    add comments, improvements to fsm.py

--- fsm.py
+++ fsm.py
@@ -4,18 +4,26 @@ class State:
   '''State base class'''
   __id = 1
   def __init__(self, name=None, match=False, description=None):
+    # a list of tuples (characterset, next_state) representing state transitions
     self.children = []
+    # a boolean, is this state terminal
     self.match = match
+    # an internal identifier, used internally graph generation
     self.id = '%s_%d' % (self.__class__.__name__, State.__id)
+    # a human-readable name
     self.name = name
+    # a human readable description
     self.description = description
+    # use the id as the name if none is supplied
     if self.name == None: 
       self.name = str(State.__id)
+    # give each state a unique id
     State.__id = State.__id + 1
 
 
   def __str__(self):
     '''a descriptive label that's used for debugging and visualization'''
+    # create a label out of name and/or description
     if self.name and self.description:
       return '%s: %s' % (self.name, self.description)
     elif self.name:
@@ -26,10 +34,12 @@ class State:
       return ''
 
   def add(self, charset, state):
+    '''add a child state'''
     self.children.append((charset, state))
 
   def __call__(self, c):
-    return [node for charset, node in self.children if c in charset]
+    '''return the set of states that are reached when @c is processed'''
+    return frozenset((node for charset, node in self.children if c in charset))
 
   def __repr__(self):
     return '%s(%s)' % (self.__class__.__name__, self.name)
