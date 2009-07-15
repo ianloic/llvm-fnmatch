8f2845e0ca49c62fc11a6daab9fe67ead759d2b1
Ian McKellar <ian@mckellar.org>
2009-07-15 12:08:46 -0400

    improve performance.py

--- performance.py
+++ performance.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+'''Test the performance of this fnmatch implementation against Python's
+built-in implementation.'''
+
+assert __name__ == '__main__'
 
 from dfa import DFA
 from nfa import NFA
@@ -13,6 +17,7 @@ from llvm.ee import *
 from llvm.passes import *
 
 def compile_native_test(compiled, pattern, path, count):
+  '''Add a native llvm function to run tests'''
   # okay, let's add a function that runs the test
   count_type = Type.int(32)
   loop_function = compiled.module.add_function(Type.function(Type.void(), []), 
@@ -76,18 +81,52 @@ def test(pattern, path, count):
     fnmatch(path, pattern)
   end_builtin = datetime.now()
 
-  print 'compile time:   %s' % (end_compile-start_compile)
-  print 'execution time: %s' % (end_execution-start_execution)
-  print 'native time:    %s' % (end_native-start_native)
-  print 'builtin time:   %s' % (end_builtin-start_builtin)
-  print ''
+  return {
+    'pattern': pattern,
+    'path': path,
+    'count': count,
+    'compile_time': (end_compile-start_compile),
+    'execution_time': (end_execution-start_execution),
+    'native_time': (end_native-start_native),
+    'builtin_time': (end_builtin-start_builtin),
+  }
+  
+  
+from optparse import OptionParser
+op = OptionParser()
+op.add_option('--format', '-F', help='output format [default: %default]', 
+    default='text', choices=('text', 'csv'))
+op.add_option('--count', '-c', default = 10000, type='int',
+    help='number of times to run each test [default: %default]')
+(options, args) = op.parse_args()
 
-TIMES = 10000
+if len(args):
+  # no arguments allowed
+  op.print_help()
+  import sys
+  sys.exit(-1)
+    
+TESTS = (
+  ('*.txt', 'README.txt'),
+  ('*.txt', 'README'),
+  ('README*', 'README.txt'),
+  ('README*', 'README'),
+  ('README', 'README'),
+)
 
-test('*.txt', 'README.txt', TIMES)
-test('*.txt', 'README', TIMES)
+results = (test(pattern, path, options.count) for pattern, path in TESTS)
 
-test('README*', 'README.txt', TIMES)
-test('README*', 'README', TIMES)
-test('README', 'README', TIMES)
+if op.format == 'text':
+  for result in results:
+    print '''fnmatch(%(path)s, %(pattern)s) %(count)d times...
+compile time:   %(compile_time)s
+execution time: %(execution_time)s
+native time:    %(native_time)s
+builtin time:   %(builtin_time)s
+''' % result
+  elif op.format == 'csv':
+    pass
 
+from pprint import pprint
+pprint(tuple(results))
+  
